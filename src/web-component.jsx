f33ccad95f113097e5c97c956c20903e65c651dc
Arnob Mahmud <arnob_t78@yahoo.com>
2025-03-17 00:36:03 +0100

    fix widget bug

--- src/web-component.jsx
+++ src/web-component.jsx
@@ -28,17 +28,37 @@
 
 // export default WidgetWebComponent;
 
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { Widget } from "./components/Widget";
 
+export const normalizeAttribute = (attribute) => {
+  return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+};
+
 class WidgetWebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
   connectedCallback() {
-    const projectId = this.getAttribute("project-id");
-    const root = createRoot(this);
-    root.render(<Widget projectId={projectId} />);
+    const props = this.getPropsFromAttributes();
+    const root = createRoot(this.shadowRoot);
+    root.render(<Widget {...props} />);
+  }
+
+  getPropsFromAttributes() {
+    const props = {};
+    for (const { name, value } of this.attributes) {
+      props[normalizeAttribute(name)] = value;
+    }
+    return props;
   }
 }
 
-customElements.define("my-widget", WidgetWebComponent);
+if (!customElements.get("my-widget")) {
+  customElements.define("my-widget", WidgetWebComponent);
+}
 
 export { WidgetWebComponent };
