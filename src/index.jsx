982a8122f462eae3b47e9becb301948608fe35b8
Arnob Mahmud <arnob_t78@yahoo.com>
2025-03-16 23:59:40 +0100

    fix WidgetWebComponent

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,7 @@
-import WidgetWebComponent from "./web-component";
+// import WidgetWebComponent from "./web-component";
+
+// customElements.define("my-widget", WidgetWebComponent);
+
+import { WidgetWebComponent } from "./web-component";
 
 customElements.define("my-widget", WidgetWebComponent);
--- src/web-component.jsx
+++ src/web-component.jsx
@@ -1,29 +1,44 @@
-import ReactDom from "react-dom/client";
+// import ReactDom from "react-dom/client";
+// import { Widget } from "./components/Widget";
+
+// export const normalizeAttribute = (attribute) => {
+//   return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+// };
+
+// class WidgetWebComponent extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.attachShadow({ mode: "open" });
+//   }
+
+//   connectedCallback() {
+//     const props = this.getPropsFromAttributes();
+//     const root = ReactDom.createRoot(this.shadowRoot);
+//     root.render(<Widget {...props} />);
+//   }
+
+//   getPropsFromAttributes() {
+//     const props = {};
+//     for (const { name, value } of this.attributes) {
+//       props[normalizeAttribute(name)] = value;
+//     }
+//     return props;
+//   }
+// }
+
+// export default WidgetWebComponent;
+
+import { createRoot } from "react-dom/client";
 import { Widget } from "./components/Widget";
 
-export const normalizeAttribute = (attribute) => {
-  return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
-};
-
 class WidgetWebComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
-
   connectedCallback() {
-    const props = this.getPropsFromAttributes();
-    const root = ReactDom.createRoot(this.shadowRoot);
-    root.render(<Widget {...props} />);
-  }
-
-  getPropsFromAttributes() {
-    const props = {};
-    for (const { name, value } of this.attributes) {
-      props[normalizeAttribute(name)] = value;
-    }
-    return props;
+    const projectId = this.getAttribute("project-id");
+    const root = createRoot(this);
+    root.render(<Widget projectId={projectId} />);
   }
 }
 
-export default WidgetWebComponent;
+customElements.define("my-widget", WidgetWebComponent);
+
+export { WidgetWebComponent };
